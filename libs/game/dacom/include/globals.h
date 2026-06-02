c0c040c4fc8ab68a5f0b662d094f053ecf99135c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-02 06:51:10 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-02

--- libs/game/dacom/include/globals.h
+++ libs/game/dacom/include/globals.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 июня 2026 14:45:50
- * Version: 1.0.65
+ * Last Updated: 02 июня 2026 06:51:09
+ * Version: 1.0.66
  */
 
 #ifndef GLOBALS_H
