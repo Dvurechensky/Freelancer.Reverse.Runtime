ae1dcc0db25212f511153c1eee321a43af549de9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-17 06:51:05 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-17

--- libs/game/dacom/include/dacom_crc.h
+++ libs/game/dacom/include/dacom_crc.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 16 июня 2026 06:51:07
- * Version: 1.0.80
+ * Last Updated: 17 июня 2026 06:51:04
+ * Version: 1.0.81
  */
 
 #ifndef DACOM_CRC_H
