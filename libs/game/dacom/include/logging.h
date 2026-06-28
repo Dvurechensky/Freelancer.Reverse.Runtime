065d7242bbbcf9ee5d433908f8e783c28e91dd7d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-06-28 11:41:23 +0300

    Auto-update headers via pre_commit.ps1 - 2026-06-28

--- libs/game/dacom/include/logging.h
+++ libs/game/dacom/include/logging.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 июня 2026 13:12:35
- * Version: 1.0.91
+ * Last Updated: 28 июня 2026 11:41:22
+ * Version: 1.0.92
  */
 
 #ifndef LOGGING_H
