e1fe542fa005ca67af8fc9f82a865012e5130123
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-01 08:34:39 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-01

--- libs/game/dacom/include/loader.h
+++ libs/game/dacom/include/loader.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 июня 2026 07:20:00
- * Version: 1.0.94
+ * Last Updated: 01 июля 2026 08:34:38
+ * Version: 1.0.95
  */
 
 #ifndef LOADER_H
