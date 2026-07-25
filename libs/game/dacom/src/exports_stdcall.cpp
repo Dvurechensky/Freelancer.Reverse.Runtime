71ed93455c04db0bc545a1154734c97c398cc936
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-25 14:28:20 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-25

--- libs/game/dacom/src/exports_stdcall.cpp
+++ libs/game/dacom/src/exports_stdcall.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 июля 2026 11:36:59
- * Version: 1.0.119
+ * Last Updated: 25 июля 2026 14:28:19
+ * Version: 1.0.120
  */
 
 #include "globals.h"
