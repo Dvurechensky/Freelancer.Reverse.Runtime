75a54029730a14cd190dd322b6d924bf90b4a503
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-07-31 16:25:32 +0300

    Auto-update headers via pre_commit.ps1 - 2026-07-31

--- libs/game/dacom/include/logstream.h
+++ libs/game/dacom/include/logstream.h
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 30 июля 2026 06:51:02
- * Version: 1.0.125
+ * Last Updated: 31 июля 2026 16:25:31
+ * Version: 1.0.126
  */
 
 #ifndef LOGSTREAM_H
